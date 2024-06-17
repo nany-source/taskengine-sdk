17f46f0108e90f4005196ee224a4ef40e0e8e922
nany <273612633@qq.com>
2024-06-17 13:54:36 +0800

    状态文本变更

--- src/Services/SendTaskStatus.php
+++ src/Services/SendTaskStatus.php
@@ -4,18 +4,13 @@ namespace TaskEngine\SDK\Services;
 
 class SendTaskStatus
 {
-    const CALLBACK_TASKSTATUS = [
-        'keepalive' => 1,
-        'complete' => 2,
-    ];
-
     /**
      * 返回任务保活的结构体
      * @return array 
      */
     public static function keepAlive()
     {
-        return ['nextStatus' => self::CALLBACK_TASKSTATUS['keepalive']];
+        return ['nextStatus' => 'keepalive'];
     }
 
     /**
@@ -24,6 +19,6 @@ class SendTaskStatus
      */
     public static function complete()
     {
-        return ['nextStatus' => self::CALLBACK_TASKSTATUS['complete']];
+        return ['nextStatus' => 'complete'];
     }
 }
